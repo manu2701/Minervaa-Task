65d2731799a2f961c430e96b0515d07ebdf1e849
Manu M <126100888+manu2701@users.noreply.github.com>
2024-11-26 10:48:09 +0530

    Final Commit

--- app.py
+++ app.py
@@ -3,13 +3,10 @@ from pydantic import BaseModel
 import joblib
 import pandas as pd
 
-# Step 3: Load the trained model
 model = joblib.load("linear_model.pkl")
 
-# Step 4: Initialize FastAPI app
 app = FastAPI()
 
-# Step 5: Define the input structure
 class PredictionRequest(BaseModel):
     Exam_1: float
     Exam_2: float
@@ -23,17 +20,17 @@ class PredictionRequest(BaseModel):
     Stress_Level: float
     Study_to_Activity_Ratio: float
 
-# Step 6: Create the predict endpoint
+# Create the predict endpoint
 @app.post("/predict")
 def predict(request: PredictionRequest):
     try:
-        # Step 7: Convert the input data to a DataFrame
+        #  Converting the input data to a DataFrame
         input_data = pd.DataFrame([request.dict()])
         
-        # Step 8: Make predictions using the loaded model
+        # Make predictions using the loaded model
         prediction = (model.predict(input_data) /10)
         
-        # Step 9: Return the prediction as a JSON response
+        # Return the prediction as a JSON response
         return {"prediction": prediction.tolist()}
     except Exception as e:
         return {"error": str(e)}
